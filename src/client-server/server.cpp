13b9907215abb05b2d7592e2511612cafaf3e3cc
Sen ZmaKi <phidelalvin@gmail.com>
2024-06-20 10:18:13 +0300

    Add concurrency with threads

--- src/client-server/server.cpp
+++ src/client-server/server.cpp
@@ -5,52 +5,56 @@
 #include <iostream>
 #include <string>
 
-void handleClient(asio::ip::tcp::socket &socket, User *user, Store *store,
-                  Catalogue *catalogue) {
+void handleClient(asio::ip::tcp::socket &socket, int userID, User *user,
+                  Store *store, Catalogue *catalogue) {
   for (;;) {
     std::cout << "\n";
     asio::error_code errorCode;
     std::string message = readFromSocket(socket, errorCode);
+    auto userIDStr = std::to_string(userID);
     if (errorCode) {
-      std::cerr << "ClientSocketError: " << errorCode.message() << "\n";
+      std::cerr << "User " << userIDStr
+                << ": ClientSocketError: " << errorCode.message() << "\n";
       return;
     }
-    std::cout << "Received payload: " << message << "\n";
+    std::cout << "User " << userIDStr << ": Received payload: " << message
+              << "\n";
     auto request = message.back();
     message.pop_back();
-    std::cout << "Decoded message: " << message << "\n";
+    std::cout << "User " << userIDStr << ": Decoded message: " << message
+              << "\n";
     switch (request) {
     case DISPLAY_REQUEST: {
-      std::cout << "Display request\n";
+      std::cout << "User " << userIDStr << ": Display request\n";
       auto catalogueStr = catalogue->display();
       writeToSocket(catalogueStr, socket);
       break;
     }
     case SEARCH_REQUEST: {
-      std::cout << "Search request\n";
+      std::cout << "User " << userIDStr << ": Search request\n";
       auto searchResult = catalogue->search(message);
       writeToSocket(searchResult, socket);
       break;
     }
     case PURCHASE_REQUEST: {
-      std::cout << "Purchase request\n";
+      std::cout << "User " << userIDStr << ": Purchase request\n";
       auto purchaseResult = catalogue->purchaseBook(user);
       writeToSocket(purchaseResult, socket);
       break;
     }
     case PAY_REQUEST: {
-      std::cout << "Pay request\n";
+      std::cout << "User " << userIDStr << ": Pay request\n";
       auto bookNumber = std::stoi(message);
       auto payResult = catalogue->payForBook(bookNumber, user, store);
       writeToSocket(payResult, socket);
       break;
     }
     case EXIT_REQUEST: {
-      std::cout << "Exit Request\n";
+      std::cout << "User " << userIDStr << ": Exit Request\n";
       return;
     }
     default: {
-      std::cout << "Unknown Request\n";
+      std::cout << "User " << userIDStr << ": Unknown Request\n";
       break;
     }
     }
@@ -64,11 +68,17 @@ int main() {
     asio::ip::tcp::acceptor acceptor(
         io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), PORT));
     std::cout << "Started server listening at port: " << PORT << "\n";
+    auto userID = 0;
     for (;;) {
       asio::ip::tcp::socket socket(io_context);
       acceptor.accept(socket);
       std::cout << "\nConnection received\n";
-      handleClient(socket, &user, &store, &catalogue);
+      userID++;
+      std::thread clientHandlerThread(
+          [&, socket = std::move(socket), user]() mutable {
+            handleClient(socket, userID, &user, &store, &catalogue);
+          });
+      clientHandlerThread.detach();
     }
   } catch (std::exception &e) {
     std::cerr << "Exception: " << e.what() << "\n";
