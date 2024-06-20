6724c1a1ad58ab2f926752540ecfc84da2dc6896
Sen ZmaKi <phidelalvin@gmail.com>
2024-06-20 06:32:15 +0300

    Improve installation script

--- Assignment-1/install.ps1
+++ Assignment-1/install.ps1
@@ -1,3 +1,10 @@
+
+function ExitIfError {
+    if ($LASTEXITCODE -ne 0) {
+      throw "Error encountered exiting.. ."
+    }
+  }
+
 function CheckAndInstallTool {
     param (
         [Parameter(Mandatory=$true)]
@@ -13,14 +20,15 @@ function CheckAndInstallTool {
         Write-Output "$toolName is not installed."
         Write-Output "Installing $toolName..."
         Invoke-Expression $installCommand
+        ExitIfError
     }
 }
+
 
 echo ""
 echo "Setting up project"
 echo "Installing C++ dev tools: clang 17.0.6+, make 4.4.1+"
 echo "Installation tools: winget, git, choco"
-echo "You also need to manually install MSVC C++ Build tools: https://github.com/bycloudai/InstallVSBuildToolsWindows"
 CheckAndInstallTool -toolName "git" -installCommand "winget install Git.Git"
 CheckAndInstallTool -toolName "clang" -installCommand "winget install LLVM.LLVM"
 CheckAndInstallTool -toolName "choco" -installCommand "winget install Chocolatey.Chocolatey"
@@ -29,12 +37,15 @@ CheckAndInstallTool -toolName "make" -installCommand "choco install make"
 echo ""
 echo "Cloning Repo"
 git clone https://github.com/SenZmaKi/Network-Programming.git
+ExitIfError
 cd Network-Programming/Assignment-1
 
 echo "Installing dependencies"
 echo "Installing asio-1.28.0"
 git clone --depth 1 --branch asio-1-28-0 https://github.com/chriskohlhoff/asio.git include/asio-1.28.0
+ExitIfError
 Remove-Item -Recurse -Force include/asio-1.28.0/.git
+ExitIfError
 
 echo ""
 echo "Setup complete"
@@ -43,6 +54,7 @@ echo "To run the server: make run_server"
 echo "To run the client: make run_client"
 echo "To run the standalone program: make run_standalone"
 echo "Clean previous builds: make clean"
+echo "You also need to manually install MSVC C++ Build tools: https://github.com/bycloudai/InstallVSBuildToolsWindows"
 try {
   Get-Command "rm" -ErrorAction Stop | Out-Null
 }
@@ -50,7 +62,7 @@ catch {
   $gitPath = (Get-Command git).Source
   $gitDir = Split-Path (Split-Path $gitPath)
   $binDir = Join-Path $gitDir "usr/bin"
-  echo "'make clean' needs the 'rm' command added to PATH. Please add the following directory to your PATH: $binDir"
+  echo "'make clean' needs the 'rm' command added to PATH. Please use git bash or add the following directory to your PATH: $binDir."
 }
 echo "Find more Make tasks at https://github.com/SenZmaKi/Network-Programming/blob/master/README.md#make"
 
