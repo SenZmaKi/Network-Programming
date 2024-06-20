62bbef294da2ef591a905e446aeb30d64f3115e4
Sen ZmaKi <phidelalvin@gmail.com>
2024-06-20 10:16:49 +0300

    Add concurrency with threads (buggy)

--- src/client-server/server.cpp
+++ src/client-server/server.cpp
@@ -1,69 +1,89 @@
 #include "../common/common.hpp"
 #include "asio.hpp"
 #include "common.hpp"
+#include <algorithm>
 #include <iostream>
 #include <string>
 
-void handleClient(asio::ip::udp::socket &socket, asio::ip::udp::endpoint &clientEndpoint, User *user, Store *store, Catalogue *catalogue) {
-  for (;;) {
-    std::cout << "\n";
-    asio::error_code errorCode;
-    std::string message = readFromSocket(socket, clientEndpoint, errorCode);
-    if (errorCode) {
-      std::cerr << "ClientSocketError: " << errorCode.message() << "\n";
-      return;
-    }
-    std::cout << "Received payload: " << message << "\n";
-    auto request = message.back();
-    message.pop_back();
-    std::cout << "Decoded message: " << message << "\n";
-    switch (request) {
-    case DISPLAY_REQUEST: {
-      std::cout << "Display request\n";
-      auto catalogueStr = catalogue->display();
-      writeToSocket(catalogueStr, socket, clientEndpoint);
-      break;
-    }
-    case SEARCH_REQUEST: {
-      std::cout << "Search request\n";
-      auto searchResult = catalogue->search(message);
-      writeToSocket(searchResult, socket, clientEndpoint);
-      break;
-    }
-    case PURCHASE_REQUEST: {
-      std::cout << "Purchase request\n";
-      auto purchaseResult = catalogue->purchaseBook(user);
-      writeToSocket(purchaseResult, socket, clientEndpoint);
-      break;
-    }
-    case PAY_REQUEST: {
-      std::cout << "Pay request\n";
-      auto bookNumber = std::stoi(message);
-      auto payResult = catalogue->payForBook(bookNumber, user, store);
-      writeToSocket(payResult, socket, clientEndpoint);
-      break;
-    }
-    case EXIT_REQUEST: {
-      std::cout << "Exit Request\n";
-      return;
-    }
-    default: {
-      std::cout << "Unknown Request\n";
-      break;
-    }
-    }
+bool handleMessage(asio::ip::udp::socket &socket,
+                   asio::ip::udp::endpoint &clientEndpoint, int userID,
+                   User *user, Store *store, Catalogue *catalogue) {
+  std::cout << "\n";
+  asio::error_code errorCode;
+  std::string message = readFromSocket(socket, clientEndpoint, errorCode);
+  auto userIDStr = std::to_string(userID);
+  if (errorCode) {
+    std::cerr << "User " << userIDStr
+              << ": ClientSocketError: " << errorCode.message() << "\n";
+    return false;
   }
+  std::cout << "User " << userIDStr << ": Received payload: " << message
+            << "\n";
+  auto request = message.back();
+  message.pop_back();
+  std::cout << "User " << userIDStr << ": Decoded message: " << message << "\n";
+  switch (request) {
+  case DISPLAY_REQUEST: {
+    std::cout << "User " << userIDStr << ": Display request\n";
+    auto catalogueStr = catalogue->display();
+    writeToSocket(catalogueStr, socket, clientEndpoint);
+    break;
+  }
+  case SEARCH_REQUEST: {
+    std::cout << "User " << userIDStr << ": Search request\n";
+    auto searchResult = catalogue->search(message);
+    writeToSocket(searchResult, socket, clientEndpoint);
+    break;
+  }
+  case PURCHASE_REQUEST: {
+    std::cout << "User " << userIDStr << ": Purchase request\n";
+    auto purchaseResult = catalogue->purchaseBook(user);
+    writeToSocket(purchaseResult, socket, clientEndpoint);
+    break;
+  }
+  case PAY_REQUEST: {
+    std::cout << "User " << userIDStr << ": Pay request\n";
+    auto bookNumber = std::stoi(message);
+    auto payResult = catalogue->payForBook(bookNumber, user, store);
+    writeToSocket(payResult, socket, clientEndpoint);
+    break;
+  }
+  case EXIT_REQUEST: {
+    std::cout << "User " << userIDStr << ": Exit Request\n";
+    return false;
+  }
+  default: {
+    std::cout << "User " << userIDStr << ": Unknown Request\n";
+    break;
+  }
+  }
+  return true;
 }
 
 int main() {
   auto [user, store, catalogue] = initDefaults();
+  auto userID = 0;
   try {
     asio::io_context io_context;
-    asio::ip::udp::socket socket(io_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), PORT));
+    asio::ip::udp::socket socket(
+        io_context, asio::ip::udp::endpoint(asio::ip::make_address(URL), PORT));
     std::cout << "Started server listening at port: " << PORT << "\n";
     for (;;) {
-      asio::ip::udp::endpoint clientEndpoint;
-      handleClient(socket, clientEndpoint, &user, &store, &catalogue);
+      auto clientEndpoint = asio::ip::udp::endpoint();
+      userID++;
+      auto userCopy = user;
+      handleMessage(socket, clientEndpoint, userID, &userCopy, &store,
+                    &catalogue);
+      auto clientHandlerThread = std::thread(
+          [&, clientEndpoint = std::move(clientEndpoint)]() mutable {
+            for (;;) {
+              if (!handleMessage(socket, clientEndpoint, userID, &userCopy,
+                                 &store, &catalogue)) {
+                break;
+              }
+            }
+          });
+      clientHandlerThread.detach();
     }
   } catch (std::exception &e) {
     std::cerr << "Exception: " << e.what() << "\n";
